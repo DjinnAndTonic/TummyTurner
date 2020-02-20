6a4115b420a2ae0896d04951c60ae7177a604f33
Brian Nguyen <briannguyen444@gmail.com>
2020-02-20 16:16:48 -0600

    Added base files
    Initiated SpringBoot files

---
+++ src/test/java/com/foodocs/TummeyTurner/TummeyTurnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodocs.TummeyTurner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TummeyTurnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
