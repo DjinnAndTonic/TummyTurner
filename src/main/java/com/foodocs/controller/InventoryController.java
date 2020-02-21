5ae47b04c9b549a921bfc133d686ddf855be5027
Brian <briannguyen444@gmail.com>
2020-02-21 14:12:28 -0600

    Get all inventory items, get item by id - dummy data

--- src/main/java/com/foodocs/controller/InventoryController.java
+++ src/main/java/com/foodocs/controller/InventoryController.java
@@ -1,14 +1,12 @@
 package com.foodocs.controller;
 
 import com.foodocs.InventoryProperties;
+import com.foodocs.core.Ingredient;
 import com.foodocs.core.MenuItem;
 import com.foodocs.service.IngredientDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -28,14 +26,26 @@ public class InventoryController {
 //    private IngredientDAO dao;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<MenuItem> getAll(@RequestParam("code") Optional<String> optional){
+    public List<Ingredient> getAll(@RequestParam("code") Optional<String> optional){
 //        String type = optional.orElse("rice");
 //        return ((core<MenuItem>)dao.findAll()).stream()
 //                .filter(p-> p.getName().equalsIgnoreCase(type)).collect(Collectors.toList());
-
-        List<MenuItem> dummyList = new ArrayList<>();
-        dummyList.add(new MenuItem(9999, "testItemName", 12, null));
+//
+        List<Ingredient> dummyList = new ArrayList<>();
+        dummyList.add(new Ingredient(1, "RICE"));
+        dummyList.add(new Ingredient(2, "GARLIC"));
+        dummyList.add(new Ingredient(3, "HAPPINESS"));
 
         return dummyList;
     }
+
+    @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Ingredient getIngredient(@PathVariable("id") Optional<String> optional){
+
+        Ingredient ingredient = new Ingredient(1, "RICE");
+
+        return ingredient;
+    }
+
+
 }
