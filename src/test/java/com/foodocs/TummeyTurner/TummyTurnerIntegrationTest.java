ec1c6827096d42a52c62330233819c9d4ba8ce28
MicroNinja95 <33522695+MicroNinja95@users.noreply.github.com>
2020-02-21 10:18:45 -0600

    started on test files

---
+++ src/test/java/com/foodocs/TummeyTurner/TummyTurnerIntegrationTest.java
@@ -0,0 +1,15 @@
+package com.foodocs.TummeyTurner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = TummeyTurnerApplication.class, webEnviroment = SpringBootTest.WebEnviroment.RANDOM_Port)
+@ActiveProfile()
+class TummeyTurnerIntegrationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
