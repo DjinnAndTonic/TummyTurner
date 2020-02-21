38cc8f579e8900f2cccd2f37c9d5e70c15970117
Brian <briannguyen444@gmail.com>
2020-02-21 14:43:44 -0600

    InventoryController with dummy data

--- src/main/java/com/foodocs/controller/InventoryController.java
+++ src/main/java/com/foodocs/controller/InventoryController.java
@@ -1,5 +1,35 @@
 package com.foodocs.controller;
 
+import com.foodocs.core.Ingredient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+
+@RestController
+@RequestMapping("/inventory")
 public class InventoryController {
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Ingredient> getAll(@RequestParam("code") Optional<String> optional){
+        List<Ingredient> dummyList = new ArrayList<>();
+        dummyList.add(new Ingredient(1, "RICE"));
+        dummyList.add(new Ingredient(2, "GARLIC"));
+        dummyList.add(new Ingredient(3, "HAPPINESS"));
+
+        return dummyList;
+    }
+
+    @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Ingredient getIngredient(@PathVariable("id") Optional<String> optional){
+
+        Ingredient ingredient = new Ingredient(1, "RICE");
+
+        return ingredient;
+    }
+
 
 }
